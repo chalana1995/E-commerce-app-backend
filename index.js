35e87574015c6f4d6f1889cb09f5f9755069b691
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-10-15 20:42:54 +0530

    connet database

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("DB Connect Successfully");
+  })
+  .catch((err) => console.log(err));
 
 app.listen(5000, () => {
   console.log("Backend Server Running");
