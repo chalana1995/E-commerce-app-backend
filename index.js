08b0e82a79ac8d1a5408bc42d817321cf9c6e9e8
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-10-18 19:58:52 +0530

    register endpoint add

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
 const app = express();
 
 dotenv.config();
@@ -13,6 +15,8 @@ mongoose
   })
   .catch((err) => console.log(err));
 
+app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+//REGISTER
+
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const saveUser = await newUser.save();
+    res.status(201).json(saveUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
