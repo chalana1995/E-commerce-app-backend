539fe5152320fe8bcd9834aeaa04f37e9d4bde91
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-10-18 22:16:31 +0530

    login part done

--- routes/auth.js
+++ routes/auth.js
@@ -22,4 +22,31 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) {
+      res.status(401).json("Wrong Credintials");
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.MONGO_SECRET
+    );
+
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    if (originalPassword !== req.body.password) {
+      res.status(401).json("Password does not match");
+    }
+
+    const { password, ...other } = user._doc;
+
+    res.json(200).json(other);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
