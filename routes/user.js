157fd69d630f1e2f2d2bf70769be2202e3425280
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-10-22 20:27:43 +0530

    add get and delete user

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,9 @@
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 const router = require("express").Router();
 
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
@@ -21,6 +25,25 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     } catch (error) {
       res.status(500).json(error);
     }
+  }
+});
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been delete");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...other } = user._doc;
+    res.json(200).json(other);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
